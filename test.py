4b0cedb2ed7649cb39506ef929a86a42a64f58f4
Arya <42996179+Nevedsi@users.noreply.github.com>
2024-03-25 16:29:30 +0700

    Update test.py (model compile)

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from tensorflow import keras
 import cv2
 import numpy as np
 
-model = keras.models.load_model('model/SIBI_ASL_Keras2.h5')
+model = keras.models.load_model('model/SIBI_ASL_Keras2.h5', compile = False)
 
 class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
 
